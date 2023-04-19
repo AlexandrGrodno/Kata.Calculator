23854f1c0800688db08364f660f373afb9525759
AlexandrGrodno <avgust_sa@mail.ru>
2023-04-19 22:40:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        Scanner string = new Scanner(System.in);
+        System.out.println("Ответ: " +calc(string.nextLine()));
+    }
+    public static String calc(String line) throws Exception {
         String[] romNumber = new String[]{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
         String[] keyRomNamber = new String[]{"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
@@ -14,79 +19,78 @@ public class Main {
         boolean secondRomeNumberFlag = false;
         boolean signFlag = false;
         StringBuilder result = new StringBuilder();
-        Scanner string = new Scanner(System.in);
-        String line = string.nextLine();
-        String[] lines = line.split(" "); // /Определение элементов строки сторки.
-          // определение римских чисел
-        for (var i = 0; i < romNumber.length; i++) {
-            if (romNumber[i].equals(lines[0])) {
-                firstNumber = i + 1;
-                firstRomeNumberFlag = true;
+            String[] lines = line.split(" "); // /Определение элементов строки .
+            if (lines.length > 3) {
+                throw new Exception("вы ввели боьшее количество значений");
             }
+            // определение римских чисел
+            for (var i = 0; i < romNumber.length; i++) {
+                if (romNumber[i].equals(lines[0])) {
+                    firstNumber = i + 1;
+                    firstRomeNumberFlag = true;
+                }
 
                 if (romNumber[i].equals(lines[2])) {
-                    secondNumber =
-                            i+ 1;
+                    secondNumber = i + 1;
                     secondRomeNumberFlag = true;
                 }
             }
             // проверка значений строки
-        if ((firstRomeNumberFlag && !secondRomeNumberFlag) || (!firstRomeNumberFlag && secondRomeNumberFlag)) {
-            throw new Exception("Вы ввели не правильные значения!");
-        }
-
-        if (!firstRomeNumberFlag &&!secondRomeNumberFlag) {
-            try {
-                firstNumber = Integer.parseInt(lines[0]);
-                secondNumber = Integer.parseInt(lines[2]);
-                } catch ( NumberFormatException e) {
-                throw new Exception("Вы ввели не правильные значения!") ;
+            if ((firstRomeNumberFlag && !secondRomeNumberFlag) || (!firstRomeNumberFlag && secondRomeNumberFlag)) {
+                throw new Exception("Вы ввели не правильные значения!");
             }
-
-        }
-        if ((firstNumber >10) || (secondNumber > 10) || (firstNumber < 1) || (secondNumber < 1)){
-        throw new Exception("Вы ввели не правильные значения!");
-        }
+            if (!firstRomeNumberFlag && !secondRomeNumberFlag) {
+                try {
+                    firstNumber = Integer.parseInt(lines[0]);
+                    secondNumber = Integer.parseInt(lines[2]);
+                } catch (NumberFormatException e) {
+                    throw new Exception("Вы ввели не правильные значения!");
+                }
+            }
+            if ((firstNumber > 10) || (secondNumber > 10) || (firstNumber < 1) || (secondNumber < 1)) {
+                throw new Exception("Вы ввели не правильные значения!");
+            }
             // Вычисление результата
-        switch (lines[1]) {
-            case "+" -> {
-                arithmeticResult = firstNumber + secondNumber;
-                signFlag = true;
+            switch (lines[1]) {
+                case "+" -> {
+                    arithmeticResult = firstNumber + secondNumber;
+                    signFlag = true;
+                }
+                case "-" -> {
+                    arithmeticResult = firstNumber - secondNumber;
+                    signFlag = true;
+                }
+                case "*" -> {
+                    arithmeticResult = firstNumber * secondNumber;
+                    signFlag = true;
+                }
+                case "/" -> {
+                    arithmeticResult = firstNumber / secondNumber;
+                    signFlag = true;
+                }
             }
-            case "-" -> {
-                arithmeticResult = firstNumber - secondNumber;
-                signFlag = true;
+            if (!signFlag) {
+                throw new Exception(" неверный знак ");
             }
-            case "*" -> {
-                arithmeticResult = firstNumber * secondNumber;
-                signFlag = true;
-            }
-            case "/" -> {
-                arithmeticResult = firstNumber / secondNumber;
-                signFlag = true;
-            }
+            if (firstRomeNumberFlag && secondRomeNumberFlag) {
+                if (arithmeticResult < 1) {
+                    throw new Exception("Результат вычисления римских чисел меньше единицы");
+                }
+                // Вычисление римских чисел
+                while (ind < keyRomNamber.length) {
+                    while (arithmeticResult >= vals[ind]) {
+                        var d = arithmeticResult / vals[ind];
+                        arithmeticResult = arithmeticResult % vals[ind];
+                        for (int i = 0; i < d; i++)
+                            result.append(keyRomNamber[ind]);
+                    }
+                    ind++;
+                }
+            } else result.append(arithmeticResult);
+
+            return result.toString();
+
+
         }
-         if (!signFlag) {
-             throw new Exception(" неверный знак ");}
-     if (firstRomeNumberFlag && secondRomeNumberFlag) {
-         if (arithmeticResult < 1) {
-             throw new Exception("Результат вычисления римских чисел меньше единицы");
-         }
-            // Вычисление римских чисел
-         while (ind < keyRomNamber.length) {
-             while (arithmeticResult >= vals[ind]) {
-                 var d = arithmeticResult / vals[ind];
-                 arithmeticResult = arithmeticResult % vals[ind];
-                 for (int i = 0; i < d; i++)
-                    result.append(keyRomNamber[ind]);
-             }
-             ind++;
-         }
-     } else result.append(arithmeticResult);
 
-     System.out.println("Ответ: " + result);
-
-
-
-    }
 }
